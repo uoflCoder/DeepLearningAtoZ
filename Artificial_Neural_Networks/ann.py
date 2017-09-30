148454f07c783f02f6c2a8d0d113f1f1f39bc479
Stephen Cornett <stephen.t.cornett@outlook.com>
2017-09-29 20:11:05 -0400

    Commented the code so it makes sense to most anyone

--- Artificial_Neural_Networks/ann.py
+++ Artificial_Neural_Networks/ann.py
@@ -17,25 +17,39 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
+#Import the dataset Churn_Modeling.csv file
 dataset = pd.read_csv('Churn_Modelling.csv')
+
+#X values, get all the rows and get relevant columns (3-12)
 X = dataset.iloc[:, 3:13].values
+
+#y value, get all the rows and get the relevant output column (13)
 y = dataset.iloc[:, 13].values
 
 # Encoding categorical data
+
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 labelencoder_X_1 = LabelEncoder()
+#Label encode the countries to be distinct integer values
 X[:, 1] = labelencoder_X_1.fit_transform(X[:, 1])
 labelencoder_X_2 = LabelEncoder()
+#Label encode male and female (1,0) (0,1)
 X[:, 2] = labelencoder_X_2.fit_transform(X[:, 2])
+#One hot encoder, transform the column Geography (1,0,0) (0,1,0) (0,0,1) to three columns
 onehotencoder = OneHotEncoder(categorical_features = [1])
 X = onehotencoder.fit_transform(X).toarray()
+
+#Get rid of the first column keep all rows because based on columns 2 and 3 you could deduce
+#column 1 without having column 1
 X = X[:, 1:]
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
+#NOTE THIS CODE, this is easy reusable code that gives in this example 80% to the training set and 20% to the test set
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
 # Feature Scaling
+#Easy feature scaling reusable code to scale the columns in the X training and X test set
 from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
