092ac1a187c5ee8a6223162d5f6dc6c1d63fa54f
Stephen Cornett <stephen.t.cornett@outlook.com>
2017-10-01 00:53:38 -0400

    Broken
    Got an error on ANN most recent, don't know why but made some progress

--- Artificial_Neural_Networks/ann.py
+++ Artificial_Neural_Networks/ann.py
@@ -42,7 +42,6 @@ X = onehotencoder.fit_transform(X).toarray()
 #Get rid of the first column keep all rows because based on columns 2 and 3 you could deduce
 #column 1 without having column 1
 X = X[:, 1:]
-
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
 #NOTE THIS CODE, this is easy reusable code that gives in this example 80% to the training set and 20% to the test set
@@ -56,7 +55,7 @@ X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
 # Part 2 - Now let's make the ANN!
-
+"""
 # Importing the Keras libraries and packages
 import keras
 from keras.models import Sequential
@@ -66,26 +65,102 @@ from keras.layers import Dense
 classifier = Sequential()
 
 # Adding the input layer and the first hidden layer
+
+#Add the first hidden layer to have 6 nodes, let the initial values be close to 0 but 'uniform' 
+#use the rectifier function ('relu') and note that the number of input features is 11
 classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu', input_dim = 11))
 
 # Adding the second hidden layer
+#Make a second hidden layer and since this is the second layer you do not need
+#to state the number of input dimension but everything else remains the same as above
 classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu'))
 
 # Adding the output layer
+#Create an output layer that has one output and give it an initial value close to 0
+#and get the activation function to be sigmoid because it is between 0 and 1 probability float value
+#and we want the probability that someone will leave the bank 
 classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
 
 # Compiling the ANN
+#This is the one I will have to research most
 classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
 
-# Fitting the ANN to the Training set
+# Fitting the ANN to the Training set, 
+#use input X_train, output y_train, batch size (10 at a time and update weights per 10), run throught the whole set 100 times
 classifier.fit(X_train, y_train, batch_size = 10, epochs = 100)
 
 # Part 3 - Making predictions and evaluating the model
 
 # Predicting the Test set results
+#Predict the values on the test results
 y_pred = classifier.predict(X_test)
+#Return a true or false value instead of probabilities
 y_pred = (y_pred > 0.5)
 
 # Making the Confusion Matrix
+#Get a 2X2 (0,0) (0,1) (1,0) (1,1) matrix that shows how many predictions you got right
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
+
+#accuracy of mine
+(1544 + 51)/2000
+
+"""
+"""
+Geography: France
+Credit Score 600
+Gender Male
+Age 40
+Tenure 3
+Balance 60000
+Number of Products 2
+Has Credit Card Yes
+Is Active Member Yes
+Estimated Salary: 50000
+
+new_prediction = classifier.predict(sc.transform(np.array([[0.000, 0.000, 600.000, 1.000, 40.000, 3.000, 60000.000, 2.000, 1.000, 1.000, 50000.000]
+])))
+"""
+
+#Part 4, evaluating, improving, and tuning the ANN
+
+#Evaluating the ANN
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import cross_val_score
+
+#Build the standard classifier to be used later in the code
+def build_classifier():
+    from keras.models import Sequential
+    from keras.layers import Dense
+
+    # Initialising the ANN
+    classifier = Sequential()
+    
+    # Adding the input layer and the first hidden layer
+    
+    #Add the first hidden layer to have 6 nodes, let the initial values be close to 0 but 'uniform' 
+    #use the rectifier function ('relu') and note that the number of input features is 11
+    classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu', input_dim = 11))
+    
+    # Adding the second hidden layer
+    #Make a second hidden layer and since this is the second layer you do not need
+    #to state the number of input dimension but everything else remains the same as above
+    classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu'))
+    
+    # Adding the output layer
+    #Create an output layer that has one output and give it an initial value close to 0
+    #and get the activation function to be sigmoid because it is between 0 and 1 probability float value
+    #and we want the probability that someone will leave the bank 
+    classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
+    
+    # Compiling the ANN
+    #This is the one I will have to research most
+    classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+    
+    return classifier
+
+#Build the KerasClassifier with a batch size of 10 and a epoch of 100 times
+classifier = KerasClassifier(build_fn = build_classifier(), batch_size = 10, nb_epoch= 100)
+#Set the accuracies to be a cross_validation using the classifier built and the X_train and y_train with a 10 fold
+#cross validation and use all cpus
+accuracies = cross_val_score(estimator = classifier, X = X_train, y = y_train, cv = 10, n_jobs = 1 )
